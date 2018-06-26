1ac3e28d82fafbcf971e71c1683863868556e566
Abdullah Saeed <abdullah.saeed@outlook.com>
2018-06-25 20:41:38 -0500

    this is the first push please work even did the process.env.PORT

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use("/notes", notes)
 app.use("/users", users)
 
 
-const port = 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
 })
